ce00264fae4e30467e656d6700fae66d1a5d383e
HieuCaoTlu <trunghieu.personal@gmail.com>
2024-02-26 21:07:21 +0700

    Install prettier, lint-staged, husky

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,14 +1,13 @@
 class NewsController {
-
     // [GET] /news
     index(req, res) {
-        res.render('news')
+        res.render('news');
     }
 
     // [GET] /news/:slug <biến ngẫu nhiên>
     show(req, res) {
-        res.send("News Detail!")
+        res.send('News Detail!');
     }
 }
 
-module.exports = new NewsController
+module.exports = new NewsController();
--- src/index.js
+++ src/index.js
@@ -1,28 +1,31 @@
-const path = require('path')
-const express = require('express')
-const morgan = require('morgan')
-const handlebars = require('express-handlebars')
-const app = express()
-const port = 3000
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
 
-const route = require('./routes')
+const route = require('./routes');
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-
-
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //Template Engine
-app.engine('hbs', handlebars({
-extname: '.hbs'
-}))
-app.set('view engine','hbs')
-app.set('views', path.join(__dirname, 'resources/views'))
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 // Routes init
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on http://localhost:${port}`)
-})
+    console.log(`Example app listening on http://localhost:${port}`);
+});
+
+//lint staged: cho phép chạy các command với file đã được add vào git
